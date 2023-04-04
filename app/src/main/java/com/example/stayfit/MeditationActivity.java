42b21eb0b5b5bf3b3c10e4ec9d81b946939c5646
yashkys <yashkhaitan02@gmail.com>
2023-04-04 23:50:31 +0530

    MeditationActivity, HomeFragment modified

--- app/src/main/java/com/example/stayfit/MeditationActivity.java
+++ app/src/main/java/com/example/stayfit/MeditationActivity.java
@@ -2,13 +2,146 @@ package com.example.stayfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.example.stayfit.databinding.ActivityMeditationBinding;
 
 public class MeditationActivity extends AppCompatActivity {
+
+    ActivityMeditationBinding binding;
+
+    String[] music = {
+            "Flute music", "Chill music", "Relax music"
+    };
+    private MediaPlayer mediaPlayer;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis ;
+    int pos = 0;
+    int cnt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meditation);
+        binding = ActivityMeditationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        cnt = 0;
+
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setLooping(true);
+
+        Spinner spinner = binding.musicSpinner;
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                pos = i;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                pos = 0;
+            }
+        });
+
+        // Create the instance of ArrayAdapter
+        // having the list of music
+        ArrayAdapter<String> adapter = new ArrayAdapter<>( this, android.R.layout.simple_spinner_item, music);
+
+        // set simple layout resource file
+        // for each item of spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spinner.setAdapter(adapter);
+
+        mButtonStartPause = binding.buttonStartPause;
+        mButtonReset = binding.buttonReset;
+
+        mButtonStartPause.setOnClickListener(v -> {
+            cnt++;
+            int hr = Integer.parseInt(binding.edtHour.getText().toString());
+            int min = Integer.parseInt(binding.edtMinutes.getText().toString());
+            int sec = Integer.parseInt(binding.edtSeconds.getText().toString());
+            mTimeLeftInMillis = (hr * 3600L + min * 60L + sec ) * 1000;
+            if ((cnt&1) != 1) {
+                mediaPlayer.stop();
+                pauseTimer();
+            } else {
+                playMusic(pos);
+                startTimer();
+            }
+        });
+
+        mButtonReset.setOnClickListener(v -> resetTimer());
+
+        updateCountDownText();
+    }
+
+    private void playMusic(int i) {
+        mediaPlayer = MediaPlayer.create(this, (i==0) ? R.raw.music1 : (i ==1) ? R.raw.music2 : R.raw.music3);
+        mediaPlayer.start();
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mediaPlayer.stop();
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = 0L;
+        mediaPlayer.stop();
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        int minutes = (int) ((mTimeLeftInMillis / 1000) / 60) % 60;
+        int hours = (int) ((mTimeLeftInMillis / 1000) / 3600) ;
+        binding.edtSeconds.setText("" + seconds);
+        binding.edtMinutes.setText("" + minutes);
+        binding.edtHour.setText("" + hours);
     }
 }
--- app/src/main/java/com/example/stayfit/fragments/HomeFragment.java
+++ app/src/main/java/com/example/stayfit/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.stayfit.fragments;
 
+import static android.content.ContentValues.TAG;
 import static android.content.Context.ALARM_SERVICE;
 
 import android.Manifest;
@@ -22,6 +23,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +33,18 @@ import android.widget.Toast;
 import com.example.stayfit.StepResetReceiver;
 import com.example.stayfit.User;
 import com.example.stayfit.databinding.FragmentHomeBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -42,9 +56,12 @@ import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
-public class HomeFragment extends Fragment  implements SensorEventListener {
+public class HomeFragment extends Fragment  { //implements SensorEventListener {
 
     private FragmentHomeBinding binding;
     TextView stepProgressText;
@@ -67,12 +84,17 @@ public class HomeFragment extends Fragment  implements SensorEventListener {
 
     User user;
 
+
+    FitnessOptions fitnessOptions = FitnessOptions.builder()
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .build();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        context = container.getContext();
 
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
@@ -93,20 +115,72 @@ public class HomeFragment extends Fragment  implements SensorEventListener {
             public void onCancelled(@NonNull DatabaseError databaseError) { }
         });
 
-
-        context = container.getContext();
-        stepProgressText = binding.stepProgressText;
-
-        if (isPermissionGranted()) {
-            requestPermission ();
-        }
-
-        loadData();
-        resetData();
-
-        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
-
+//
+//        stepProgressText = binding.stepProgressText;
+//
+//        if (isPermissionGranted()) {
+//            requestPermission ();
+//        }
+//
+//        loadData();
+//        resetData();
+//
+//        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+        GoogleSignInAccount account = GoogleSignIn.getAccountForExtension(context, fitnessOptions);
+        Fitness.getRecordingClient(context, account)
+                .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener(aVoid -> {
+                    // Permission granted. Now you can read step count data.
+                    readStepCount();
+                })
+                .addOnFailureListener(e -> {
+                    // Permission not granted.
+                    Log.e(TAG, "Failed to subscribe for step count", e);
+                });
         return view;
+    }
+
+    private void readStepCount() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        long endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.DAY_OF_YEAR, -7);
+        long startTime = calendar.getTimeInMillis();
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        GoogleSignInAccount account = GoogleSignIn.getAccountForExtension(context, fitnessOptions);
+        Fitness.getHistoryClient(context, account)
+                .readData(readRequest)
+                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
+                    @Override
+                    public void onSuccess(DataReadResponse dataReadResponse) {
+                        // Process the data read from Google Fit.
+                        List<DataSet> dataSets = dataReadResponse.getDataSets();
+                        if (dataSets.size() > 0) {
+                            for (DataSet dataSet : dataSets) {
+                                for (DataPoint dataPoint : dataSet.getDataPoints()) {
+                                    long startTime = dataPoint.getStartTime(TimeUnit.MILLISECONDS);
+                                    long endTime = dataPoint.getEndTime(TimeUnit.MILLISECONDS);
+                                    int steps = dataPoint.getValue(Field.FIELD_STEPS).asInt();
+                                    // Do something with the step count data.
+                                    stepProgressText.setText("" + steps);
+                                }
+                            }
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to read step count data.
+                        Log.e(TAG, "Failed to read step count", e);
+                    }
+                });
+
     }
 
     private void loadUserData() {
@@ -152,132 +226,120 @@ public class HomeFragment extends Fragment  implements SensorEventListener {
 
     }
 
-    private void resetData() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-
-        Intent intent = new Intent(context, StepResetReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_MUTABLE);
-
-        AlarmManager alarmManager = (AlarmManager) requireActivity().getSystemService(ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-
-    }
-
-    private void loadData() {
-        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("step", Context.MODE_PRIVATE);
-        previousTotalSteps = sharedPreferences.getFloat("currentstep", 0f);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        running = true;
-
-        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
-
-        //most recent mobiles and major android mobile have this sensor
-        Sensor countSensor = sensorManager.getDefaultSensor (Sensor.TYPE_STEP_COUNTER);
-        //this also in very few
-        Sensor detectorSensor = sensorManager.getDefaultSensor (Sensor.TYPE_STEP_DETECTOR);
-        //below sensor for particularly samsung and moto
-        Sensor accelerometer = sensorManager.getDefaultSensor (Sensor.TYPE_ACCELEROMETER);
-
-        //now check which one is in your mobile
-        if (countSensor != null) {
-            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-        } else if (detectorSensor != null) {
-            sensorManager.registerListener(this, detectorSensor, SensorManager.SENSOR_DELAY_UI);
-        } else if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
-        } else {
-            Toast.makeText(getContext(), "Your device is not compatible", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        running = false;
-        sensorManager.unregisterListener(this);
-    }
-
-    private void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            ActivityCompat.requestPermissions(requireActivity(),
-                    new String[] {android.Manifest.permission.ACTIVITY_RECOGNITION},
-                    ACTIVITY_RECOGNITION_REQUEST_CODE);
-        }
-    }
-    private boolean isPermissionGranted() {
-        //check the user have permission enabled
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            return ContextCompat.checkSelfPermission(context, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED;
-        }
-        return false;
-    }
+//    private void resetData() {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis(System.currentTimeMillis());
+//        calendar.set(Calendar.HOUR_OF_DAY, 0);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
 //
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if (requestCode == ACTIVITY_RECOGNITION_REQUEST_CODE){
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                // Permission granted
-//            } else {
-//                // Permission denied
-//            }
-//        }        // Add more cases if needed
+//        Intent intent = new Intent(context, StepResetReceiver.class);
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_MUTABLE);
+//
+//        AlarmManager alarmManager = (AlarmManager) requireActivity().getSystemService(ALARM_SERVICE);
+//        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+//
+//    }
+//
+//    private void loadData() {
+//        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("step", Context.MODE_PRIVATE);
+//        previousTotalSteps = sharedPreferences.getFloat("currentstep", 0f);
 //    }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        running = true;
+//
+//        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+//
+//        //most recent mobiles and major android mobile have this sensor
+//        Sensor countSensor = sensorManager.getDefaultSensor (Sensor.TYPE_STEP_COUNTER);
+//        //this also in very few
+//        Sensor detectorSensor = sensorManager.getDefaultSensor (Sensor.TYPE_STEP_DETECTOR);
+//        //below sensor for particularly samsung and moto
+//        Sensor accelerometer = sensorManager.getDefaultSensor (Sensor.TYPE_ACCELEROMETER);
+//
+//        //now check which one is in your mobile
+//        if (countSensor != null) {
+//            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+//        } else if (detectorSensor != null) {
+//            sensorManager.registerListener(this, detectorSensor, SensorManager.SENSOR_DELAY_UI);
+//        } else if (accelerometer != null) {
+//            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+//        } else {
+//            Toast.makeText(getContext(), "Your device is not compatible", Toast.LENGTH_LONG).show();
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        running = false;
+//        sensorManager.unregisterListener(this);
+//    }
 
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            //we need to detect the magnitude
-            float xaccel = event.values[0];
-            float yaccel = event.values[1];
-            float zaccel = event.values[2];
-            double magnitude = Math.sqrt((xaccel * xaccel + yaccel * yaccel + zaccel * zaccel));
+//    private void requestPermission() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            ActivityCompat.requestPermissions(requireActivity(),
+//                    new String[] {android.Manifest.permission.ACTIVITY_RECOGNITION},
+//                    ACTIVITY_RECOGNITION_REQUEST_CODE);
+//        }
+//    }
+//    private boolean isPermissionGranted() {
+//        //check the user have permission enabled
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            return ContextCompat.checkSelfPermission(context, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED;
+//        }
+//        return false;
+//    }
 
-            double magnitudeDelta = magnitude - magnitudePreviousStep;
-            magnitudePreviousStep = magnitude;
-
-            if(magnitudeDelta > 6){
-                totalSteps++;
-            }
-            int steps = (int)totalSteps;
-            stepProgressText.setText("" + totalSteps);
-            calorieBurned.setText("" + getCalorieCount(steps));
-            distanceMoved.setText("" + getDistance(steps));
-
-
-        }else {
-            if (running) {
-                totalSteps = event.values[0];
-                int currentStep = (int) (totalSteps - previousTotalSteps);
-                stepProgressText.setText("" + currentStep);
-                calorieBurned.setText("" + getCalorieCount(currentStep));
-                distanceMoved.setText("" + getDistance(currentStep));
-            }
-        }
-
-    }
-
-    private int getDistance(int steps) {
-        return steps/1300;
-    }
-
-    private int getCalorieCount(int steps) {
-        return steps/13;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
+    //   @Override
+//    public void onSensorChanged(SensorEvent event) {
+//
+//        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+//            //we need to detect the magnitude
+//            float xaccel = event.values[0];
+//            float yaccel = event.values[1];
+//            float zaccel = event.values[2];
+//            double magnitude = Math.sqrt((xaccel * xaccel + yaccel * yaccel + zaccel * zaccel));
+//
+//            double magnitudeDelta = magnitude - magnitudePreviousStep;
+//            magnitudePreviousStep = magnitude;
+//
+//            if(magnitudeDelta > 6){
+//                totalSteps++;
+//            }
+//            int steps = (int)totalSteps;
+//            stepProgressText.setText("" + totalSteps);
+//            calorieBurned.setText("" + getCalorieCount(steps));
+//            distanceMoved.setText("" + getDistance(steps));
+//
+//
+//        }else {
+//            if (running) {
+//                totalSteps = event.values[0];
+//                int currentStep = (int) (totalSteps - previousTotalSteps);
+//                stepProgressText.setText("" + currentStep);
+//                calorieBurned.setText("" + getCalorieCount(currentStep));
+//                distanceMoved.setText("" + getDistance(currentStep));
+//            }
+//        }
+//
+//    }
+//
+//    private int getDistance(int steps) {
+//        return steps/1300;
+//    }
+//
+//    private int getCalorieCount(int steps) {
+//        return steps/13;
+//    }
+//
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int i) {
+//
+//    }
 
 }
