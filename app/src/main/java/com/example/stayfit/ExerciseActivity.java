82dbfe57f8d43a6b02e0628054a03932cde14c23
yashkys <yashkhaitan02@gmail.com>
2023-04-05 19:06:31 +0530

    added and designed RecipieActivity and ExerciseActivity

--- app/src/main/java/com/example/stayfit/ExerciseActivity.java
+++ app/src/main/java/com/example/stayfit/ExerciseActivity.java
@@ -3,6 +3,9 @@ package com.example.stayfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ExerciseActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ExerciseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+        WebView webView = findViewById(R.id.exercise_web_view);
+        String url = "https://www.healthline.com/nutrition/workout-routine-for-men";
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/stayfit/RecipeActivity.java
+++ app/src/main/java/com/example/stayfit/RecipeActivity.java
@@ -3,12 +3,27 @@ package com.example.stayfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class RecipeActivity extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        webView = findViewById(R.id.recipe_web_view);
+
+        String url = "https://www.eatingwell.com/recipes/18045/weight-loss-diet/";
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
     }
 }
